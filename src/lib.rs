6a4ba060f9f68449500bb24f04999d4b711e6c7e
Puru Kulkarni <puruk@protonmail.com>
2021-10-13 17:31:35 -0700

    lib: Create the KcapiError, KcapiHandle types
    We need data structures to hold and rustify the errors which the
    libkcapi may throw at us. Additionally, libkcapi defines the
    opaque struct kcapi_handle, which we need to allocate and implement
    correctly in our bindings for this library.
    
    Therefore, This change:
    
    * Defines the KcapiError type to hold errors from libkcapi
    * Defines the KcapiHandle struct and implements a constructor for
      it in order to allocate it in a rusty way.
    
    Note: We need to redefine the kcapi_handle in this crate as the
    member _unused is private in kcapi-sys crate. This is a hack for
    now which we will resolve later.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,76 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+/*
+ * $Id$
+ *
+ * Copyright (c) 2021, Purushottam A. Kulkarni.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and
+ * or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE
+ *
+ */
+
+use std::fmt;
+
+const BITS_PER_BYTE: usize = 8;
+
+pub type KcapiResult<T> = std::result::Result<T, KcapiError>;
+
+#[derive(Debug, Clone)]
+pub struct KcapiError {
+    code: i64,
+    message: String,
+}
+
+impl fmt::Display for KcapiError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} ({})", &self.message, &self.code)
     }
 }
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy)]
+struct kcapi_handle {
+    _unused: [u8; 0],
+}
+
+#[derive(Debug, Clone)]
+pub struct KcapiHandle {
+    algorithm: String,
+    handle: *mut kcapi_sys::kcapi_handle,
+}
+
+impl KcapiHandle {
+    fn new(alg: &str) -> Self {
+        let handle = Box::into_raw(Box::new(kcapi_handle { _unused: [0u8; 0] }))
+            as *mut kcapi_sys::kcapi_handle;
+        KcapiHandle {
+            algorithm: alg.to_string(),
+            handle,
+        }
+    }
+}
+
+mod test;
